c3b3c68b0c5d7bc198dcd9bfe1f875521e2c89a6
Tom Morrell <tmorrell@caltech.edu>
2022-11-18 20:46:54 +0000

    Bug fix for loop

--- scripts/change_owner.py
+++ scripts/change_owner.py
@@ -5,16 +5,16 @@ from invenio_accounts import current_accounts
 from invenio_utilities_tuw.utils import get_identity_for_user, get_record_service, get_user_by_identifier
 from invenio_utilities_tuw.cli.utils import set_record_owners
 
-pids = ['a7f64-a8k10']
-owners = [360]
+pids = ['pybpv-w7661','dhfgs-p7319']
+owners = [430]
 
 u = get_identity_for_user(2)
 service = get_record_service()
 
 for pid in pids:
     record = service.read(id_=pid, identity=u)._record
-    owners = [get_user_by_identifier(owner) for owner in owners]
-    set_record_owners(record, owners)
+    all_owners = [get_user_by_identifier(owner) for owner in owners]
+    set_record_owners(record, all_owners)
     if service.indexer:
         service.indexer.index(record)
 
