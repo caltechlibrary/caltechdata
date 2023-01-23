52df6bc9f841ce8dee4aaba983afd4dec0e85199
Tom Morrell <tmorrell@caltech.edu>
2023-01-23 15:38:07 -0800

    Working version of add file

--- scripts/add_file.py
+++ scripts/add_file.py
@@ -1,67 +1,32 @@
 import click, os
-from io import SEEK_END, SEEK_SET
 
 from flask.cli import with_appcontext
-from invenio_cache import current_cache
 from invenio_db import db
 from invenio_files_rest.models import ObjectVersion
-from invenio_pidstore.models import PersistentIdentifier
-from invenio_records.api import Record
-from invenio_records.models import RecordMetadata
-from invenio_utilities_tuw.utils import get_identity_for_user, get_record_service, get_user_by_identifier
-from invenio_utilities_tuw.cli.utils import set_record_owners
-
-
-identity = get_identity_for_user(2)
-service = get_record_service()
-
-
-#from invenio_pidstore.resolver import Resolver
-
-#record_resolver = Resolver(
-#    pid_type='recid', object_type='rec', getter=ZenodoRecord.get_record
-#)
-#
-
-#def get_service():
-#    factory = lambda: current_rdm_records.records_service
-#    return factory()
+from invenio_utilities_tuw.utils import get_identity_for_user, get_record_service
 
 @click.command('add_file')
 @click.argument('recid', type=str)
 @click.argument('fp', type=click.File('rb'))
-@click.option('--replace-existing', '-f', is_flag=True, default=False)
+@click.argument('user', type=int)
 @with_appcontext
-def add_file(recid, fp, replace_existing):
+def add_file(recid, fp, user):
     """Add a new file to a published record."""
-    #pid, record = record_resolver.resolve(recid)
-    #service = get_service()
-    record = service.read(id_=recid, identity=identity)._record
+    identity = get_identity_for_user(user)
+    service = get_record_service()
+    record = service.read(id_=recid,identity=identity)._record
     bucket = record.files.bucket
     key = os.path.basename(fp.name)
 
     obj = ObjectVersion.get(bucket, key)
-    if obj is not None and not replace_existing:
-        click.echo(click.style(u'File with key "{key}" already exists.'
-                   u' Use `--replace-existing/-f` to overwrite it.'.format(
-                        key=key, recid=recid), fg='red'))
-        return
-
-    fp.seek(SEEK_SET, SEEK_END)
-    size = fp.tell()
-    fp.seek(SEEK_SET)
-
-    print(record)
 
     click.echo(u'Will add the following file:\n')
     click.echo(click.style(
         u'  key: "{key}"\n'
         u'  bucket: {bucket}\n'
-        u'  size: {size}\n'
         u''.format(
             key=key,
-            bucket=bucket.id,
-            size=size),
+            bucket=bucket.id),
         fg='green'))
     click.echo(u'to record:\n')
     click.echo(click.style(
@@ -73,7 +38,7 @@ def add_file(recid, fp, replace_existing):
             title=record['metadata']['title'],
             uuid=record.id),
         fg='green'))
-    if replace_existing and obj is not None:
+    if obj is not None:
         click.echo(u'and remove the file:\n')
         click.echo(click.style(
             u'  key: "{key}"\n'
@@ -87,13 +52,9 @@ def add_file(recid, fp, replace_existing):
 
     if click.confirm(u'Continue?'):
         bucket.locked = False
-        if obj is not None and replace_existing:
-            bucket.size -= obj.file.size
-            ObjectVersion.delete(bucket, obj.key)
-        ObjectVersion.create(bucket, key, stream=fp, size=size)
+        record.files[key] = fp
         bucket.locked = True
 
-        record.files.flush()
         record.commit()
         db.session.commit()
         click.echo(click.style(u'File added successfully.', fg='green'))
