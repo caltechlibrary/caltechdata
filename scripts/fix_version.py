6de4a891b0f3403646806ddcd0c69bbb7ae42131
Tom Morrell <tmorrell@caltech.edu>
2023-05-12 18:10:48 +0000

    Working version script

--- scripts/fix_version.py
+++ scripts/fix_version.py
@@ -5,11 +5,19 @@ from invenio_accounts import current_accounts
 from invenio_utilities_tuw.utils import get_identity_for_user, get_record_service
 
 
-pids = ['brzrq-t3789']
+pids = ['ct5kt-cv878']
 
 u = get_identity_for_user(2)
 service = get_record_service()
 
 for pid in pids:
-    version = service.new_version(id_=pid, identity=u)
-    print(version._record)
+    record = service.record_cls.pid.resolve(pid)
+    print(record.versions)
+    record.versions.clear_next()
+    record.commit()
+    db.session.commit()
+    service.indexer.index(record)
+    print(record.versions)
+    #print(service.read_draft(id_=pid, identity=u)._record)
+    #version = service.new_version(id_=pid, identity=u)
+    #print(version._record)
