4c2bca00a9ec8b9f83ff6e3ae1140b9f05195a4f
Tom Morrell <tmorrell@caltech.edu>
2026-04-22 16:28:43 +0000

    Remove references field

---
+++ assets/js/invenio_app_rdm/overridableRegistry/mapping.js
@@ -0,0 +1,13 @@
+// This file is part of InvenioRDM
+// Copyright (C) 2023 CERN.
+//
+// Invenio App RDM is free software; you can redistribute it and/or modify it
+// under the terms of the MIT License; see LICENSE file for more details.
+
+/**
+ * Add here all the overridden components of your app.
+ */
+
+export const overriddenComponents = {
+	"InvenioAppRdm.Deposit.AccordionFieldReferences.container": () => null,
+};
